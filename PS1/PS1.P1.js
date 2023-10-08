bb87e9609ac822a8c2700b1d6f5a5dace9c90b84
Alex Miller <amiller06880@gmail.com>
2023-10-08 14:13:43 -0400

    Completed PS1

---
+++ PS1/PS1.P1.js
@@ -0,0 +1,2 @@
+const reverse = string => string.split('').sort().reverse().join('')
+console.log(`Testing the reverse function on supercalifragilisticexpialidocious: ${reverse('supercalifragilisticexpialidocious')}`)
